4bf25d5f09333bc2e36d86d36c2419a7f24cbd1c
jaanbaaz <jaanbaaz.akhtar@esmagico.in>
2025-01-06 12:34:53 +0530

    session import added to server queries

--- db/server.py
+++ db/server.py
@@ -1,4 +1,4 @@
-import dotenv
+
 import os
 ##
 from sqlalchemy.future import select
@@ -17,12 +17,27 @@ from datetime import datetime
 from sqlalchemy import cast, String ,and_
 from sqlalchemy.dialects.postgresql import ARRAY
 from .models import Issues, CommunityOrgs, PointSystem, PrHistory
+from dotenv import load_dotenv
 
 
+load_dotenv(".env")
+
+
+def get_postgres_uri():
+    DB_HOST = os.getenv('POSTGRES_DB_HOST')
+    DB_NAME = os.getenv('POSTGRES_DB_NAME')
+    DB_USER = os.getenv('POSTGRES_DB_USER')
+    DB_PASS = os.getenv('POSTGRES_DB_PASS')
+
+    return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
     
 class ServerQueries:
-    def __init__(self, postgres_orm: PostgresORM):
-        self.session = postgres_orm.session
+    def __init__(self):
+        DATABASE_URL = get_postgres_uri()         
+        # Initialize Async SQLAlchemy
+        engine = create_async_engine(DATABASE_URL, echo=False,poolclass=NullPool)
+        async_session = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+        self.session = async_session
         
     def convert_dict(self,data):
         try:
